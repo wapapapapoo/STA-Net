86c4959f0e9c0b25a172805188e3ff33faae0600
wapapapapoo <wapapapapoo@users.noreply.github.com>
2026-03-06 16:41:22 +0800

    fix bug

--- preprocessing/window.py
+++ preprocessing/window.py
@@ -117,7 +117,7 @@ for subject in subject_list:
     save_dir = r'data/model_input'
     save_name = subject
 
-    os.makedirs(save_dir)
+    os.makedirs(save_dir, exist_ok=True)
     np.savez(os.path.join(save_dir,save_name),**save_dict)
     print('\n==============save {} success=============\n'.format(save_name)) 
 
