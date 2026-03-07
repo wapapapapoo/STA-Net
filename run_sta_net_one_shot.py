2be0c1c89b5c976bf0314301a175d2ea7ae8804b
wapapapapoo <wapapapapoo@users.noreply.github.com>
2026-03-07 20:33:34 +0800

    run cpu

--- run_sta_net_one_shot.py
+++ run_sta_net_one_shot.py
@@ -7,10 +7,10 @@ from keras import layers
 from keras.callbacks import ModelCheckpoint
 import os
 
-
-gpus = tf.config.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+tf.config.set_visible_devices([], 'GPU')
+# gpus = tf.config.list_physical_devices('GPU')
+# for gpu in gpus:
+#     tf.config.experimental.set_memory_growth(gpu, True)
 
 class targetacccallback(keras.callbacks.Callback):
     def __init__(self, target_acc):
