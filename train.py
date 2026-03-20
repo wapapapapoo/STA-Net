d7cd52957a4137f6fc8b0410a8307f1ed3228416
wapapapapoo <wapapapapoo@users.noreply.github.com>
2026-03-20 17:46:34 +0800

    patch

--- train.py
+++ train.py
@@ -187,7 +187,7 @@ def train(model, train_loader, val_loader, args):
 
     loss_fn = LossModule()
 
-    for epoch in range(20):
+    for epoch in range(40):
 
         train_loss = train_epoch(
             epoch,
